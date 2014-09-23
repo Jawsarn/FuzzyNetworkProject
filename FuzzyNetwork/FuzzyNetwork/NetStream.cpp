a0cf5d1999f8148f08e08163f25b10c302fa6efa
Jawsarn <christian_markowicz@hotmail.com>
2014-09-23 17:44:32 +0200

    added netstream with some things :D

--- FuzzyNetwork/FuzzyNetwork/NetStream.cpp
+++ FuzzyNetwork/FuzzyNetwork/NetStream.cpp
@@ -9,3 +9,24 @@ NetStream::NetStream()
 NetStream::~NetStream()
 {
 }
+
+
+int Read(int p_BytesToRead, void* p_Buffer)
+{
+
+}
+
+int Write(int p_BytesToWrite, const void* p_Buffer)
+{
+
+}
+
+bool SetCurPos(int p_Pos)
+{
+
+}
+
+int GetCurPos()
+{
+
+}
--- FuzzyNetwork/FuzzyNetwork/NetStream.h
+++ FuzzyNetwork/FuzzyNetwork/NetStream.h
@@ -6,5 +6,13 @@ class NetStream:Stream
 public:
 	NetStream();
 	~NetStream();
+
+	
+
+	virtual int Read(int p_BytesToRead, void* p_Buffer);
+	virtual int Write(int p_BytesToWrite, const void* p_Buffer);
+
+	virtual bool SetCurPos(int p_Pos);
+	virtual int GetCurPos();
 };
 
